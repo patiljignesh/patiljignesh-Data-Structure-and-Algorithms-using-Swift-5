4b6c3eb7fd042d92620a67f1292a5d2cd65e41a1
Jignesh Patil <patiljignesh@gmail.com>
2021-07-20 18:53:17 +0200

    Create Big O Notation example

--- Data Structure and Algorithms using Swift 5.playground/Pages/Big O Notation.xcplaygroundpage/Contents.swift
+++ Data Structure and Algorithms using Swift 5.playground/Pages/Big O Notation.xcplaygroundpage/Contents.swift
@@ -2,6 +2,23 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
 
-//: [Next](@next)
+func searchForJoe(_ arr: [String]){
+    let before = Date()
+    
+    for i in 0..<arr.count {
+        if arr[i] == "Joe" {
+            print("Found Joe at\(i)")
+        }
+    }
+    
+    let after = Date()
+    let component = Calendar.current.dateComponents([.nanosecond], from: before, to: after)
+    let milliSeconds : Double = Double(component.nanosecond! / 1000000)
+    print("Finding Joe took: \(milliSeconds) milliseconds. ")
+}
+
+
+// let joe = ["Joe"]
+let joe = Array<String>(repeating: "", count: 1000000)
+searchForJoe(joe)
